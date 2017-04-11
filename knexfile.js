cd4085f3800b097daa22140e3546affa5ffa5902
Crystal Tate <crys.tate@icloud.com>
2017-04-11 14:42:10 -0500

    set up knex

--- knexfile.js
+++ knexfile.js
@@ -1,6 +1,23 @@
+// Update with your config settings.
+
 module.exports = {
+
   development: {
     client: 'pg',
     connection: 'postgres://localhost/snacker'
-  }
+  },
+
+  test: {
+    client: 'pg',
+    connection: 'postgres://localhost/snacker-test'
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user:     'username',
+      password: 'password'
+    }
+
 };
--- migrations/20170411141734_snacks.js
+++ migrations/20170411141734_snacks.js
@@ -1,8 +1,13 @@
-
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("snacks", function(table){
+  	table.increments().primary();
+  	table.string("name");
+    table.text("img_url", "longtext");
+  	table.text("review_description");
+    table.bigInteger("rating");
+  });
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTable("snacks");
 };
